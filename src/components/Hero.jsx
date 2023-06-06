3fe2f653e93c2912e58af61a0e782ac83e9fbe43
Guillaume Flambard <56681566+zoanlogia@users.noreply.github.com>
2023-06-06 19:03:22 +0700

    ready main

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import styles from "../constants/style";
 import saas from "../assets/saas.svg";
 import GetStarted from "./GetStarted";
-import { heroCity } from "../assets/index.js";
+import { handToEndPng } from "../assets/index.js";
 import { useTheme } from "../hooks/useTheme.jsx";
 import { Link } from "react-router-dom";
 
@@ -67,7 +67,7 @@ const Hero = () => {
         } md:my-10 relative`}
       >
         <img
-          src={heroCity}
+          src={handToEndPng}
           alt="robot"
           className="w-[100%] h-[100%] relative z-index-[5]"
         />
