518febef5ad79b52cc56f811cd5ff510af745032
Guillaume Flambard <56681566+zoanlogia@users.noreply.github.com>
2023-06-27 19:06:15 +0800

    rea

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,15 +1,15 @@
-import s1 from "../assets/images/s1.png";
-import s2 from "../assets/images/s2.png";
-import shuffle from "../assets/images/shuffle.png";
-import star from "../assets/images/star.svg";
-import shield from "../assets/images/shield.svg";
-import send from "../assets/images/send.svg";
-import afcp from "../assets/images/afcp.jpeg";
-import renape from "../assets/images/renape.jpeg";
-import cemka from "../assets/images/cemka.jpeg";
-import cro from "../assets/images/cro.png";
-import linkedin from "../assets/images/linkedin.svg";
-import twitter from "../assets/images/twitter.svg";
+import s1 from "../../src/assets/images/s1.png";
+import s2 from "../../src/assets/images/s2.png";
+import shuffle from "../../src/assets/images/shuffle.png";
+import star from "../../src/assets/images/star.svg";
+import shield from "../../src/assets/images/shield.svg";
+import send from "../../src/assets/images/send.svg";
+import afcp from "../../src/assets/images/afcp.jpeg";
+import renape from "../../src/assets/images/renape.jpeg";
+import cemka from "../../src/assets/images/cemka.jpeg";
+import cro from "../../src/assets/images/cro.png";
+import linkedin from "../../src/assets/images/linkedin.svg";
+import twitter from "../../src/assets/images/twitter.svg";
 
 
 import { LOCALES } from "../i18n/locales";
