c001cca3c1049dc1956a67597756cc57e1f15039
Guillaume Flambard <56681566+zoanlogia@users.noreply.github.com>
2023-07-06 10:05:45 +0800

    Updates

--- assets/CardText-0bbc66fe.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-6c449266.js";const a=({children:r})=>e.jsx("div",{className:"feature-card p-5 rounded-xl hover:opacity-100",children:r});export{a as default};
---
+++ assets/CardText-8f730ac8.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-04f38f72.js";const a=({children:r})=>e.jsx("div",{className:"feature-card p-5 rounded-xl hover:opacity-100",children:r});export{a as default};
